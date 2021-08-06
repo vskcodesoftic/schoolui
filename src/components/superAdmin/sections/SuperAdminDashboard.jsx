41ecf159374dfbc5ec599f04b96159e98d9a5aac
vskcodesoftic <vegisiva@codesoftic.com>
2021-08-06 17:44:21 +0530

    done till super admin part

--- src/components/superAdmin/sections/SuperAdminDashboard.jsx
+++ src/components/superAdmin/sections/SuperAdminDashboard.jsx
@@ -1,57 +1,56 @@
 import React from 'react'
-
-const SuperAdminDashboard = () => {
+import Navbar from '../../Navbar'
+import SideNav from '../SideNav'
+import AttendenceCard from './Cards/AttendenceCard'
+import ExpensesChart from './Charts/ExpensesChart'
+import IncomeChart from './Charts/IncomeChart'
+import FeesOverview from './FeesOverview'
+import Header from '../Header'
+import LibarayOverview from './LibarayOverview'
+const SuperAdminDashboard = (props) => {
     return (
         <>
-        <div class="grid grid-flow-col grid-cols-4 grid-rows-1 gap-2 px-2 py-2">
- 
-        <div className="md:col-span-1 bg-green-500 px-2 py-2">
-               red
-               <div className="grid grid-flow-col grid-col-1 md:grid-rows-5  sm:grid-rows-5 gap-2 px-2 py-2">
-                      <div className="p-2">
-                          <button className="bg-red-500 rounded-3xl px-2">link1</button>
-                      </div>
+        <Navbar />
+        
+        
 
-                      <div className="p-2">
-                          <button className="bg-red-500 rounded-3xl px-2">link1</button>
-                      </div>
-                      <div className="p-2">
-                          <button className="bg-red-500 rounded-3xl px-2">link1</button>
-                      </div>
-                      <div className="p-2">
-                          <button className="bg-red-500 rounded-3xl px-2">link1</button>
-                      </div>
-                       <div className="p-2">
-                          <button className="bg-red-500 rounded-3xl px-2">link1</button>
-                      </div>
-               </div>
-           </div>
-           
-        <div className="md:col-span-3 sm:col-span-3 bg-orange-400 px-2 py-2">
-               
-               <div className="grid grid-flow-col grid-col-4 md:grid-rows-1  sm:grid-rows-1 gap-2 px-2 py-2">
-                      <div className="p-5 bg-red-400">
-                          hello
-                      </div>
+      
+        <div class="grid grid-flow-col grid-col-4 ">
+               <SideNav />
+                <div className="grid col-span-3">
+                            <div className="">
+                                   <Header />
+                           
+                            <div className="p-2 grid grid-flow-col gap-2">
+                                            <div className="flex bg-white p-2 my-2 flex justify-center shadow-lg">
+                                                <IncomeChart />
+                                            </div>
+                                            <div className="flex  p-2 my-2  flex justify-center shadow-lg">
+                                                <ExpensesChart />
+                                            </div>
+                            
+                            </div>
 
-                      <div className="p-5 bg-pink-400">
-                          hello
-                      </div>
-
-                      <div className="p-5 bg-yellow-400">
-                          hello
-                      </div>
-
-                      <div className="p-5 bg-blue-400">
-                          hello
-                      </div>
-               </div>
-        </div>
- 
-         
-
-        </div>
- 
+                            <div className="p-2 grid grid-flow-col gap-2">
+                                            <div className="flex p-2 my-2">
+                                                <AttendenceCard/>
+                                            </div>
+                                            <div className="flex p-2 my-2">
+                                                <FeesOverview />
+                                            </div>
+                                            <div className="flex p-2 my-2">
+                                                <LibarayOverview />
+                                            </div>
+                            </div>
+                        
+                             
+                     
+                         </div>
+                    
+                            
+                            </div>
+                </div>
+        
         </>
     )
 }
