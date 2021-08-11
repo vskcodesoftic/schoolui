542ea5326c2e869837094c8feccc566b56028ed0
vskcodesoftic <vegisiva@codesoftic.com>
2021-08-11 18:41:30 +0530

    aaded till attedence data

--- src/components/superAdmin/DataTables/ViewData/AdminData.js
+++ src/components/superAdmin/DataTables/ViewData/AdminData.js
@@ -1,11 +1,68 @@
+import MaterialTable from 'material-table';
 import React from 'react'
+import { AddBox, ArrowDownward } from "@material-ui/icons";
 
-const AdminData = () => {
+const AdminData =() =>
+ {
+    const { useState } = React;
+  
+    const [columns, setColumns] = useState([
+      { title: 'Name', field: 'name' },
+      { title: 'Surname', field: 'surname', initialEditValue: 'initial edit value' },
+      { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+      {
+        title: 'Birth Place',
+        field: 'birthCity',
+        lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+      },
+    ]);
+  
+    const [data, setData] = useState([
+      { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+      { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
+    ]);
+  
     return (
-        <div>
-            <h1>admin data</h1>
-        </div>
+      <MaterialTable
+        title="AdminData Preview"
+        columns={columns}
+        data={data}
+        AdminData={{
+          onRowAdd: newData =>
+            new Promise((resolve, reject) => {
+              setTimeout(() => {
+                setData([...data, newData]);
+                
+                resolve();
+              }, 1000)
+            }),
+          onRowUpdate: (newData, oldData) =>
+            new Promise((resolve, reject) => {
+              setTimeout(() => {
+                const dataUpdate = [...data];
+                const index = oldData.tableData.id;
+                dataUpdate[index] = newData;
+                setData([...dataUpdate]);
+  
+                resolve();
+              }, 1000)
+            }),
+          onRowDelete: oldData =>
+            new Promise((resolve, reject) => {
+              setTimeout(() => {
+                const dataDelete = [...data];
+                const index = oldData.tableData.id;
+                dataDelete.splice(index, 1);
+                setData([...dataDelete]);
+                
+                resolve()
+              }, 1000)
+            }),
+        }}
+      />
     )
-}
+  }
 
-export default AdminData
+  
+
+  export default AdminData;
